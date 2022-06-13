0f2bf2a32bafdb723e49b0b2d80b534f3088e875
zak <90650199+zkayia@users.noreply.github.com>
2022-06-13 20:50:14 +0200

    fix: maintain HttpClient for all nekosama requests

--- lib/provider/api.dart
+++ lib/provider/api.dart
@@ -1,6 +1,9 @@
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nekodroid/provider/http_client.dart';
 import 'package:nekosama_dart/nekosama_dart.dart';
 
 
-final apiProvider = StateProvider<NekoSama>((ref) => NekoSama());
+final apiProvider = Provider<NekoSama>(
+	(ref) => NekoSama(httpClient: ref.watch(httpClientProvider)),
+);
---
+++ lib/provider/http_client.dart
@@ -0,0 +1,7 @@
+
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final httpClientProvider = Provider<HttpClient>((ref) => HttpClient());
