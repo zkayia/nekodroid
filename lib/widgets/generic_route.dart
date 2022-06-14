fa241a7f45ee960fee23b052654926b2e2ab81ba
zak <90650199+zkayia@users.noreply.github.com>
2022-06-14 22:08:40 +0200

    feat: generic route added leading/trailing widgets

--- lib/widgets/generic_route.dart
+++ lib/widgets/generic_route.dart
@@ -8,12 +8,16 @@ import 'package:nekodroid/widgets/single_line_text.dart';
 class GenericRoute extends StatelessWidget {
 
 	final String? title;
+	final Widget? leading;
+	final Widget? trailing;
 	final Widget? body;
 	final Future<bool> Function(BuildContext context)? onExitTap;
 	
 	const GenericRoute({
 		super.key,
 		this.title,
+		this.leading,
+		this.trailing,
 		this.body,
 		this.onExitTap,
 	});
@@ -45,25 +49,73 @@ class GenericRoute extends StatelessWidget {
 					),
 				),
 				extendBodyBehindAppBar: true,
-				appBar: title != null
-					? PreferredSize(
-						preferredSize: const Size.fromHeight(kTopBarHeight + kPaddingSecond),
-						child: Card(
-								margin: const EdgeInsets.only(
-									top: kPaddingSecond,
-									left: kPaddingSecond,
-									right: kPaddingSecond,
-								),
-								child: Center(
-									child: SingleLineText(
-										title ?? "",
-										style: Theme.of(context).textTheme.titleMedium,
-									),
-								),
-							),
+				appBar: [title, leading, trailing].any((e) => e != null)
+					? _GenericRouteAppbar(
+						title: title, 
+						leading: leading, 
+						trailing: trailing,
 					)
 					: null,
 				body: body,
+			),
+		),
+	);
+}
+
+class _GenericRouteAppbar extends StatelessWidget implements PreferredSizeWidget {
+	
+	final String? title;
+	final Widget? leading;
+	final Widget? trailing;
+
+	const _GenericRouteAppbar({
+		required this.title,
+		required this.leading,
+		required this.trailing,
+	});
+
+	@override
+  Size get preferredSize => const Size.fromHeight(kTopBarHeight + kPaddingSecond);
+
+	@override
+	Widget build(BuildContext context) => Card(
+		margin: const EdgeInsets.only(
+			top: kPaddingSecond,
+			left: kPaddingSecond,
+			right: kPaddingSecond,
+		),
+		child: ConstrainedBox(
+			constraints: BoxConstraints.tight(const Size.fromHeight(kTopBarHeight)),
+			child: Stack(
+				children: [
+					if (title != null)
+						Center(
+							child: Padding(
+								padding: const EdgeInsets.symmetric(horizontal: kPaddingSecond),
+								child: SingleLineText(
+									title!,
+									textAlign: TextAlign.center,
+									style: Theme.of(context).textTheme.titleMedium,
+								),
+							),
+						),
+					if (leading != null)
+						Align(
+							alignment: Alignment.centerLeft,
+							child: Padding(
+								padding: const EdgeInsets.symmetric(horizontal: kPaddingSecond),
+								child: leading,
+							),
+						),
+					if (trailing != null)
+						Align(
+							alignment: Alignment.centerRight,
+							child: Padding(
+								padding: const EdgeInsets.symmetric(horizontal: kPaddingSecond),
+								child: trailing,
+							),
+						),
+				],
 			),
 		),
 	);
