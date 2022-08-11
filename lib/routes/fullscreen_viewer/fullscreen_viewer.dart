374299023babc941095faf1187952ee0074880f0
zak <90650199+zkayia@users.noreply.github.com>
2022-08-12 00:39:22 +0200

    added scroll support to fullscreen_viewer

--- lib/routes/fullscreen_viewer/fullscreen_viewer.dart
+++ lib/routes/fullscreen_viewer/fullscreen_viewer.dart
@@ -4,15 +4,44 @@ import 'package:nekodroid/constants.dart';
 import 'package:nekodroid/widgets/generic_route.dart';
 
 
+/* CONSTANTS */
+
+
+
+
+/* MODELS */
+
+
+
+
+/* PROVIDERS */
+
+
+
+
+/* MISC */
+
+
+
+
+/* WIDGETS */
+
 class FullscreenViewerRoute extends StatelessWidget {
 
 	const FullscreenViewerRoute({super.key});
 
 	@override
 	Widget build(BuildContext context) => GenericRoute(
-		body: Padding(
-			padding: const EdgeInsets.all(kPaddingMain),
-			child: Center(
+		body: ConstrainedBox(
+			constraints: BoxConstraints.tight(MediaQuery.of(context).size),
+			child: SingleChildScrollView(
+				physics: kDefaultScrollPhysics,
+				padding: const EdgeInsets.only(
+					top: kPaddingSecond,
+					left: kPaddingSecond,
+					right: kPaddingSecond,
+					bottom: kPaddingSecond + kFabSize + 16,
+				),
 				child: ModalRoute.of(context)!.settings.arguments as Widget,
 			),
 		),
