76e1d7022a06388fe1809d3fcdc74ed46076b9c9
zak <90650199+zkayia@users.noreply.github.com>
2022-10-12 20:43:54 +0200

    added rangevalues serialisation

--- lib/extensions/range_values.dart
+++ lib/extensions/range_values.dart
@@ -1,8 +1,24 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 
 extension RangeValuesX on RangeValues {
 
   String prettyToString() => "${start.toStringAsFixed(2)} - ${end.toStringAsFixed(2)}";
+
+  Map<String, double> toMap() => {
+    "start": start,
+    "end": end,
+  };
+  
+  static RangeValues fromMap(Map<String, double> map) => RangeValues(
+    map["start"] ?? 0,
+    map["end"] ?? 0,
+  );
+
+  String toJson() => jsonEncode(toMap());
+
+  static RangeValues fromJson(String json) => fromMap(jsonDecode(json));
 }
