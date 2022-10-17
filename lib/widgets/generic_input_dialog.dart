31df7c956cf4ad5484618597b0db7480712e6a1b
zak <90650199+zkayia@users.noreply.github.com>
2022-10-17 14:01:05 +0200

    added initialtext to inputDialog

--- lib/widgets/generic_input_dialog.dart
+++ lib/widgets/generic_input_dialog.dart
@@ -4,8 +4,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nekodroid/widgets/generic_dialog.dart';
 
 
-final _inputControllerProv = StateProvider.autoDispose<TextEditingController>(
-  (ref) => TextEditingController(),
+final _inputControllerProv = StateProvider.autoDispose.family<
+  TextEditingController,
+  String?
+>(
+  (ref, initial) => TextEditingController(text: initial),
 );
 
 final _errorTextProv = StateProvider.autoDispose<String?>(
@@ -15,11 +18,13 @@ final _errorTextProv = StateProvider.autoDispose<String?>(
 class GenericInputDialog extends ConsumerWidget {
 
   final String title;
+  final String? initialText;
   final String? hintText;
   final String? Function(String? value, BuildContext context)? validator;
   
   const GenericInputDialog({
     required this.title,
+    this.initialText,
     this.hintText,
     this.validator,
     super.key,
@@ -30,7 +35,7 @@ class GenericInputDialog extends ConsumerWidget {
     title: title,
     onConfirm: (context) => _exit(context, ref),
     child: TextFormField(
-      controller: ref.watch(_inputControllerProv),
+      controller: ref.watch(_inputControllerProv(initialText)),
       autofocus: true,
       autovalidateMode: AutovalidateMode.onUserInteraction,
       validator: validator != null
@@ -49,7 +54,7 @@ class GenericInputDialog extends ConsumerWidget {
   );
 
   String? _exit(BuildContext context, WidgetRef ref, [bool exit=false]) {
-    final text = ref.read(_inputControllerProv).text.trim();
+    final text = ref.read(_inputControllerProv(initialText)).text.trim();
     final validatorResult = validator?.call(text, context);
     ref.read(_errorTextProv.notifier).update((_) => validatorResult);
     if (validatorResult != null) {
