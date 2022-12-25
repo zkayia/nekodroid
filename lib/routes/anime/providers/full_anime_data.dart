09a0856becbbdac6e5efb10a3df0c8ff4291f18a
zak <90650199+zkayia@users.noreply.github.com>
2022-12-25 23:20:20 +0100

    fixed anime prov not updating when anime was not in db

--- lib/routes/anime/providers/full_anime_data.dart
+++ lib/routes/anime/providers/full_anime_data.dart
@@ -11,31 +11,45 @@ import 'package:nekodroid/schemas/isar_episode_status.dart';
 final fullAnimeDataProv = StreamProvider.autoDispose.family<FullAnimeData, Uri>(
   (ref, url) async* {
     final anime = await ref.watch(apiProv).getAnime(url);
+    final stringUrl = url.toString();
     final isar = Isar.getInstance()!;
-    final initialIsarAnime = await isar.isarAnimeListItems.getByUrl(url.toString());
+    var initialIsarAnime = await isar.isarAnimeListItems.getByUrl(stringUrl);
     await initialIsarAnime?.episodeStatuses.load();
     yield FullAnimeData(anime: anime, isarAnime: initialIsarAnime);
-    if (initialIsarAnime != null) {
-      final updatesGroup = StreamGroup();
-      ref.onDispose(updatesGroup.close);
-      updatesGroup.add(isar.isarAnimeListItems.watchObjectLazy(initialIsarAnime.id));
-      final epIdsInGroup = <int>[];
-      for (final ep in initialIsarAnime.episodeStatuses) {
-        updatesGroup.add(isar.isarEpisodeStatus.watchObjectLazy(ep.id));
-        epIdsInGroup.add(ep.id);
-      }
-      await for (final _ in updatesGroup.stream) {
-        final isarAnime = await isar.isarAnimeListItems.get(initialIsarAnime.id);
-        await isarAnime?.episodeStatuses.load();
-        for (final ep in isarAnime?.episodeStatuses ?? const {}) {
-          if (!epIdsInGroup.contains(ep.id)) {
-            updatesGroup.add(isar.isarEpisodeStatus.watchObjectLazy(ep.id));
-            epIdsInGroup.add(ep.id);
-          }
-        }
-        yield FullAnimeData(anime: anime, isarAnime: isarAnime);
-      }
-      updatesGroup.close();
+    if (initialIsarAnime == null) {
+      await isar.isarAnimeListItems.watchLazy().firstWhere(
+        (_) {
+          initialIsarAnime = isar.isarAnimeListItems.getByUrlSync(stringUrl);
+          return initialIsarAnime != null;
+        },
+      );
     }
+    await initialIsarAnime?.episodeStatuses.load();
+    yield FullAnimeData(anime: anime, isarAnime: initialIsarAnime);
+    if (initialIsarAnime == null) {
+      // To be safe in case the watcher streams ends before a non null anime is found
+      // Should never happen as watcher steams are endless throughout the app lifetime
+      return;
+    }
+    final updatesGroup = StreamGroup();
+    ref.onDispose(updatesGroup.close);
+    updatesGroup.add(isar.isarAnimeListItems.watchObjectLazy(initialIsarAnime!.id));
+    final epIdsInGroup = <int>[];
+    for (final ep in initialIsarAnime!.episodeStatuses) {
+      updatesGroup.add(isar.isarEpisodeStatus.watchObjectLazy(ep.id));
+      epIdsInGroup.add(ep.id);
+    }
+    await for (final _ in updatesGroup.stream) {
+      final isarAnime = await isar.isarAnimeListItems.get(initialIsarAnime!.id);
+      await isarAnime?.episodeStatuses.load();
+      for (final ep in isarAnime?.episodeStatuses ?? const {}) {
+        if (!epIdsInGroup.contains(ep.id)) {
+          updatesGroup.add(isar.isarEpisodeStatus.watchObjectLazy(ep.id));
+          epIdsInGroup.add(ep.id);
+        }
+      }
+      yield FullAnimeData(anime: anime, isarAnime: isarAnime);
+    }
+    updatesGroup.close();
   },
 );
